bbc27d85f31a9229837a8442d2c11f0f839f2e8c
Gunwoo Cho <whitesky0109@naver.com>
2025-09-24 20:55:40 +0900

    [main] preview_image 파일 오동작 수정

--- src/component/shareButton/index.tsx
+++ src/component/shareButton/index.tsx
@@ -38,7 +38,7 @@ export const ShareButton = () => {
                 "//" +
                 window.location.host +
                 baseUrl +
-                "/preview_image.png",
+                "/preview_image.jpg",
               link: {
                 mobileWebUrl:
                   window.location.protocol +
